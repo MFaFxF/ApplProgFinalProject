3bdcab4cb8d18eea5532c2e6804ed31fef45d1da
MFaFxF <max.freudenstein@protonmail.com>
2025-07-07 23:30:06 +0200

    feat: print data in MVM

--- final_project/viewmodel/mainViewModel.py
+++ final_project/viewmodel/mainViewModel.py
@@ -1,5 +1,11 @@
+import time
+
 from PyQt5.QtCore import QObject, pyqtSignal, QTimer
 import numpy as np
+from PyQt5.QtWidgets import QApplication
+
+from final_project.service.signal_processor import SignalProcessor
+
 
 class MainViewModel(QObject):
     """"""
@@ -10,3 +16,12 @@ class MainViewModel(QObject):
     def __init__(self):
         """"""
         super().__init__()
+        self.signalProcessor = SignalProcessor()
+        self.signalProcessor.get_signal()
+
+
+if __name__ == '__main__':
+    mainViewModel = MainViewModel()
+    while True:
+        time.sleep(1)
+        print(mainViewModel.signalProcessor.data[0])
