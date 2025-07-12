8fe021489185e3aa5a07b576dfbc568f64e01265
MFaFxF <max.freudenstein@8select.de>
2025-07-12 15:46:39 +0200

    x axis

--- final_project_again/mainView/lifePlotView.py
+++ final_project_again/mainView/lifePlotView.py
@@ -18,10 +18,12 @@ class LivePlotWidget(QWidget):
         grid.spacing = 0
         grid.border_color = 'white'
 
+        # Title
         title = scene.Label("Plot Title GRID", color='white')
         title.height_max = 40
         grid.add_widget(title, row=0, col=0, col_span=2)
 
+        # Y Axis (left)
         yaxis = scene.AxisWidget(
             orientation='left',
             axis_label='Y Axis',
@@ -31,14 +33,30 @@ class LivePlotWidget(QWidget):
         )
         yaxis.width_max = 80
         yaxis.border_color = 'white'
-        grid.add_widget(yaxis, row=1, col=0)  # FIXED
+        grid.add_widget(yaxis, row=1, col=0)
 
-        self.view = grid.add_view(row=1, col=1)  # FIXED
-        # self.view.camera = 'panzoom'
+        # View (main plot)
+        self.view = grid.add_view(row=1, col=1)
+        self.view.camera = 'panzoom'
+        self.view.border_color = 'white'
         yaxis.link_view(self.view)
 
+        # X Axis (bottom)
+        xaxis = scene.AxisWidget(
+            orientation='bottom',
+            axis_label='Time',
+            axis_font_size=12,
+            axis_label_margin=30,
+            tick_label_margin=5
+        )
+        xaxis.height_max = 40
+        xaxis.border_color = 'white'
+        grid.add_widget(xaxis, row=2, col=1)
+        xaxis.link_view(self.view)
+
+        # Line plot
         self.line = scene.Line(np.array([[0, 0]]), parent=self.view.scene)
-        self.view.camera.set_range(x=(-100, 10000), y=(-100000, 100000))
+        self.view.camera.set_range(x=(0, 10000), y=(-100000, 100000))
 
     def update_data(self, time_axis, data):
         print("Updating data in LivePlotWidget")
